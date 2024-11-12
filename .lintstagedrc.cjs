4f635db317ca9d11a2377d4fd484832779b7ec8e
why <42405527+fistSea@users.noreply.github.com>
2024-11-12 22:03:52 +0800

    feat: 去除eslint

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{js,cjs,mjs,jsx,ts,cts,mts,tsx,vue}': 'eslint --fix --cache',
-  '*.{scss,css,vue}': 'stylelint  --fix --allow-empty-input',
-  '*.{ts,cts,mts,tsx,vue}': () => 'vue-tsc --noEmit -p tsconfig.vitest.json --composite false',
-}
+  "*.{}": "eslint --fix --cache",
+  "*.{}": "stylelint  --fix --allow-empty-input",
+  "*.{}": () => "vue-tsc --noEmit -p tsconfig.vitest.json --composite false",
+};
